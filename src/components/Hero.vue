619c302b2543e00dd44f9f8325293e1f823d8b89
agus <agustinolivieri30@gmail.com>
2025-03-12 12:24:35 -0300

    sin tacc logo

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,7 +9,9 @@ defineProps({
     <section id="home" class="bg-emerald-700 py-20 mb-4 text-white">
         <div class="max-w-7xl mx-auto px-4 flex flex-col items-center">
             <div class="text-center">
-                <h1 class="text-4xl font-extrabold">{{ title }}</h1>
+                <h1 class="text-4xl font-extrabold">{{ title }}
+                    <span class="inline-flex items-center ml-2"><img src="@/assets/images/sintacc-logo.png" width="46" /></span>
+                </h1>
                 <p class="my-4 text-xl">{{ subtitle }}</p>
             </div>
         </div>
