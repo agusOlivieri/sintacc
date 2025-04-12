3c92b613feb42d5c6d5e21d9f3caae6c6f78c845
agus <agustinolivieri30@gmail.com>
2025-04-12 13:51:49 -0300

    changing Hero styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,12 +10,13 @@ defineProps({
     <section id="home" class="bg-gradient-to-r from-emerald-700 to-emerald-600 py-20 text-white">
         <div class="max-w-4xl mx-auto px-6 text-center">
             
-            <div class="flex felx-col items-center justify-center gap-4">
-                <h1 class="text-2xl font-extrabold sm:text-5xl">{{ title }}</h1>
-                <img src="@/assets/images/gluten-free.png" class="w-20 sm:w-32 transition-transform duration-300 hover:scale-110" alt="Gluten Free Logo" />
+            <div class="flex flex-col items-center justify-center gap-4">
+                <h1 class="mb-6 text-2xl font-extrabold sm:text-5xl">{{ title }}</h1>
+                <!-- <img src="@/assets/images/delicias_sintacc_removedbg.png" class="w-20 sm:w-64 transition-transform duration-300 hover:scale-110" alt="Gluten Free Logo" /> -->
+                <img src="@/assets/images/delicias_sintacc_removedbg.png" class="rounded-full w-28 sm:w-56" />
+                <p class="mt-6 text-lg sm:text-2xl leading-relaxed max-w-2xl mx-auto">{{ subtitle }}</p>
             </div>
-            
-            <p class="mt-6 text-lg sm:text-2xl opacity-90 leading-relaxed max-w-2xl mx-auto">{{ subtitle }}</p>
+
         </div>
     </section>
 </template>
