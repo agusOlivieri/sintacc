23ad9ef3b7fd26cb36e4c9cf40f84f74e2f436f9
agus <agustinolivieri30@gmail.com>
2025-04-12 17:03:00 -0300

    changed logo animation on Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,11 +7,17 @@ defineProps({
 document.addEventListener('DOMContentLoaded', () => {
   const logo = document.getElementById('logo-delicias');
 
+  logo.classList.add('animate-expand-vertically');
+
   setTimeout(() => {
     console.log("animation ended")
-    logo.classList.remove('animate-expand-vertically');
     logo.classList.add('animate-tada');
+    logo.classList.remove('animate-expand-vertically');
   }, 1000); 
+
+  setTimeout(() => {
+    logo.classList.remove('animate-tada');
+  }, 2000)
 });
 </script>
 
@@ -21,10 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
         <div class="max-w-4xl mx-auto px-6 text-center">
             
             <div class="flex flex-col items-center justify-center gap-4">
-                <h1 class="mb-6 text-2xl font-extrabold sm:text-5xl animate-slide-up-fade animate-duration-600 animate-delay-400">{{ title }}</h1>
+                <h1 class="mb-2 text-2xl font-extrabold sm:text-5xl animate-slide-up-fade animate-duration-600 animate-delay-400">{{ title }}</h1>
                 <!-- <img src="@/assets/images/delicias_sintacc_removedbg.png" class="w-20 sm:w-64 transition-transform duration-300 hover:scale-110" alt="Gluten Free Logo" /> -->
-                <img id="logo-delicias" src="@/assets/images/delicias_sintacc_removedbg.png" class="rounded-full w-28 sm:w-56 animate-expand-vertically" />
-                <p class="mt-6 text-lg sm:text-2xl leading-relaxed max-w-2xl mx-auto animate-slide-up-fade animate-duration-600 animate-delay-400">{{ subtitle }}</p>
+                <img id="logo-delicias" src="@/assets/images/delicias_sintacc_removedbg.png" class="rounded-full w-36 sm:w-56 hover:animate-tada" />
+                <p class="mt-2 text-lg sm:text-2xl leading-relaxed max-w-2xl mx-auto animate-slide-up-fade animate-duration-600 animate-delay-400">{{ subtitle }}</p>
             </div>
 
         </div>
